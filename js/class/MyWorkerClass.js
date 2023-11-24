c908d0a4daf4088ec2fcccb1b433505740dbb182
Simona Sud <ssimona.sud@gmail.com>
2023-11-24 08:48:30 +0200

    private properties

--- js/class/MyWorkerClass.js
+++ js/class/MyWorkerClass.js
@@ -1,28 +1,28 @@
 "use strict";
 
 class MyWorker extends Employee {
-  hoursWorked = 0;
-  hourlyPay;
+  #hoursWorked = 0;
+  #hourlyPay;
   constructor(name, lastName, dollarPerHour) {
     // iskviesti tevines klases konstukroriu
     super(name, lastName);
-    this.hourlyPay = dollarPerHour;
+    this.#hourlyPay = dollarPerHour;
   }
 
   // metoda work(howMany)
   work(howMany) {
     console.log(`-- ${this.firstName} worked for ${howMany} hours`);
-    this.hoursWorked += howMany;
+    this.#hoursWorked += howMany;
   }
 
   // metoda calPay
   calcPay() {
     console.log("skaiciuojam alga");
     //paskaiciuoti kiek gaunasi isdirbtas val padauginus is valandinio
-    const alga = this.hoursWorked * this.hourlyPay;
+    const alga = this.#hoursWorked * this.#hourlyPay;
     console.log(`-- ${this.firstName} ismketi ${alga.toFixed(2)} eur`);
     //nunulinti valandas
-    this.hoursWorked = 0;
+    this.#hoursWorked = 0;
     return alga;
   }
 }
